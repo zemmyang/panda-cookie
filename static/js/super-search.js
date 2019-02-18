448ebd3e633e224eb837d7b5c607340241b58cf9
Zemmy A <zemmy.ang@gmail.com>
2019-02-19 00:35:47 +0200

    some testing

--- static/js/super-search.js
+++ static/js/super-search.js
@@ -2,6 +2,8 @@
     Author: Kushagra Gour (http://kushagragour.in)
     MIT Licensed
 */
+---
+---
 
 (function () {
     var isSearchOpen = false,
@@ -63,7 +65,7 @@
     }
 
     var xmlhttp=new XMLHttpRequest();
-    xmlhttp.open("GET","/sitemap.xml");
+    xmlhttp.open("GET","{{ site.baseurl }}/sitemap.xml");
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState != 4) return;
         if (xmlhttp.status != 200 && xmlhttp.status != 304) { return; }
